28bca524142e4d47c8bf23ab9e0e4734e3814690
JC BERGOT <18658617+Brettdah@users.noreply.github.com>
2021-05-05 12:24:06 +0200

    Fix semantics, moved the build of squashed images, and add cleaning for builder image

--- get-updated-glibc.sh
+++ get-updated-glibc.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --squash -t "brettdah/centos6-novsyscall:1.0.0" .
-
-echo "Here come the builds :"
-docker build --squash -t "brettdah/centos6-novsyscall:1.0.0" .
-
 echo "building builder image to extract all rpms"
 docker build --target centos-with-vsyscall -t "builder:1" .
 
@@ -17,10 +12,15 @@ then
 fi
 
 # Geting the ID of the container
-container=docker ps | grep "test1" | awk '{print $1}'
+container=$(docker ps | grep "test1" | awk '{print $1}')
 
 # Copying the rpms into local folder
 echo "getting the rpms"
 docker cp ${container}:/rpms/x86_64/ ./files/rpms
 
+echo "Here come the builds :"
+docker build --squash -t "brettdah/centos6-novsyscall:1.0.0" .
+
+docker stop ${container}
 docker rm ${container}
+docker rmi builder:1
